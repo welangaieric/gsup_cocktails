d50f2f96a877585ddf667ddbca13267e5e9702a6
welangai <welangaieric@gmail.com>
2025-10-02 12:03:17 +0300

    added about section and implemented animations on its elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         })
         navTween.fromTo('nav',{backgroundColor:'transparent'},{
             backgroundColor:'#00000050',
-            backgroundFilter:'blur(10px)',
+            backdropFilter:'blur(10px)',
             duration:1,
             ease:'power1.inOut'
         }) 
